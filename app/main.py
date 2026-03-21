ebcfd8604182c52513bb7325148d2164c8cf575b
hersh doshi <hershdoshi55@gmail.com>
2026-03-21 02:37:57 -0700

    added route to fetch latest parcel

--- app/main.py
+++ app/main.py
@@ -1,12 +1,66 @@
-
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+from typing import Any
 
 app = FastAPI()
 
-
-@app.get("/shipment")
-def get_shipment():
-    return {
-        "content" : "wooden table",
+shipments = {
+    12701: {
+        "weight" : 1.2,
+        "content" : "glassware",
+        "status" : "placed"
+    },
+    12702: {
+        "weight" : 3.5,
+        "content" : "electronics",
         "status" : "in transit"
+    },
+    12703: {
+        "weight" : 0.8,
+        "content" : "books",
+        "status" : "delivered"
+    },
+    12704: {
+        "weight" : 12.0,
+        "content" : "wooden table",
+        "status" : "placed"
+    },
+    12705: {
+        "weight" : 2.3,
+        "content" : "clothing",
+        "status" : "in transit"
+    },
+    12706: {
+        "weight" : 5.7,
+        "content" : "kitchenware",
+        "status" : "out for delivery"
+    },
+    12707: {
+        "weight" : 0.3,
+        "content" : "jewelry",
+        "status" : "delivered"
     }
+}
+
+
+@app.get("/shipment/latest")
+def get_latest_shipment() -> dict[str, Any]:
+    id = max(shipments.keys())
+    return shipments[id]
+
+
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]:
+
+    if id not in shipments:
+        return {"detail" : "Given id doesn't exist!"}
+    return shipments[id]
+
+
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
