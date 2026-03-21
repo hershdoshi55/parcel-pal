35339e04b56db30ade9f5d8c1a227512f01ad052
hersh doshi <hershdoshi55@gmail.com>
2026-03-21 05:11:42 -0700

    added in the patch and delete method

--- app/main.py
+++ app/main.py
@@ -5,53 +5,25 @@ from typing import Any
 app = FastAPI()
 
 shipments = {
-    12701: {
-        "weight" : 1.2,
-        "content" : "glassware",
-        "status" : "placed"
-    },
-    12702: {
-        "weight" : 3.5,
-        "content" : "electronics",
-        "status" : "in transit"
-    },
-    12703: {
-        "weight" : 0.8,
-        "content" : "books",
-        "status" : "delivered"
-    },
-    12704: {
-        "weight" : 12.0,
-        "content" : "wooden table",
-        "status" : "placed"
-    },
-    12705: {
-        "weight" : 2.3,
-        "content" : "clothing",
-        "status" : "in transit"
-    },
-    12706: {
-        "weight" : 5.7,
-        "content" : "kitchenware",
-        "status" : "out for delivery"
-    },
-    12707: {
-        "weight" : 0.3,
-        "content" : "jewelry",
-        "status" : "delivered"
-    }
+    12701: {"weight": 1.2, "content": "glassware", "status": "placed"},
+    12702: {"weight": 3.5, "content": "electronics", "status": "in transit"},
+    12703: {"weight": 0.8, "content": "books", "status": "delivered"},
+    12704: {"weight": 12.0, "content": "wooden table", "status": "placed"},
+    12705: {"weight": 2.3, "content": "clothing", "status": "in transit"},
+    12706: {"weight": 5.7, "content": "kitchenware", "status": "out for delivery"},
+    12707: {"weight": 0.3, "content": "jewelry", "status": "delivered"},
 }
 
 
 @app.get("/shipment")
 def get_shipment(id: int) -> dict[str, Any]:
-    #check for shipment with given id
+    # check for shipment with given id
     if id not in shipments:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Given id doesn't exist"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Given id doesn't exist"
         )
     return shipments[id]
+
 
 @app.post("/shipment")
 def submit_shipment(content: str, weight: float) -> dict[str, int]:
@@ -59,18 +31,47 @@ def submit_shipment(content: str, weight: float) -> dict[str, int]:
     if weight > 25:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail="Maximum weight limit is 25 kgs"
+            detail="Maximum weight limit is 25 kgs",
         )
-    
+
     new_id = max(shipments.keys()) + 1
-    
+
     shipments[new_id] = {
-        "content" : content,
-        "weight" : weight,
-        "status" : "placed",
+        "content": content,
+        "weight": weight,
+        "status": "placed",
     }
-    
+
     return {"id": new_id}
+
+@app.put("/shipment")
+def shipment_update(
+    id: int, content: str, weight: float, status: str
+) -> dict[str, Any]:
+    shipments[id] = {
+        "weight": weight, 
+        "content": content, 
+        "status": status,
+    }
+
+    return shipments[id]
+
+@app.patch("/shipment")
+def patch_shipment(id: int, body: dict[str, Any]):
+    shipment = shipments[id]
+    shipment.update(body)
+    shipments[id] = shipment
+
+    return shipment
+
+@app.delete("/shipment")
+def delete_shipment(id: int) -> dict[str, str]:
+    shipments.pop(id)
+    return {"detail": f"shipment with id #{id} is deleted!"}
+
+
+
+
 
 
 
